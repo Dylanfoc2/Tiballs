1ee49f8d9cd6bffc9862d2a202c268f79b800686
Dylanfoc <144268338+Dylanfoc2@users.noreply.github.com>
2025-10-31 20:05:51 +0100

    Update Tiballs loader.lua

--- Tiballs loader.lua
+++ Tiballs loader.lua
@@ -1,3 +1,3 @@
-if game.GameId == 155615604 then
+if game.GameId == 73885730 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Dylanfoc2/Tiballs/refs/heads/main/155615604.lua",true))()
 end
